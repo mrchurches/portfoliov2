f0ef7d252e05c2f300c6a59959eef34fe66c0ce9
mrchurches <laureanoiglesias34@gmail.com>
2024-02-07 23:50:10 -0300

    add es.json to texts, improve home, add first breakpoints

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
-import Image from "next/image";
-
+import texts from "../../public/es.json"
 export default function Home() {
+
   return (
-<div className="flex items-center w-full h-full">
-  <div className="relative w-full h-full flex justify-between align-center">
-    <div className="h-20 w-20" id="deformed-circle"></div>
-    <Image className="" src="/personal-home.jpg" width='800' height='500' alt="personal image of the owner of the portfolio" />
-  </div>
-  <div className="flex ml-4 absolute align-center items-center w-full flex-col">
-    <h1 id="description-title">Full stack dev</h1>
-    <div id="text" className="bg-orange-300 p-6 w-80 mr-40">Un desarrollador Full Stack con conocimientos en Node.js y React está capacitado para crear aplicaciones web tanto en el frontend como en el backend. En el lado del servidor, utiliza Node.js para desarrollar APIs y servicios web eficientes y escalables. En el lado del cliente, utiliza React para construir interfaces de usuario interactivas y dinámicas. En resumen, esta persona tiene la capacidad de trabajar en todas las partes esenciales de una aplicación web, desde la manipulación de datos y la lógica del servidor hasta la presentación y la experiencia del usuario en el navegador.</div>
-  </div>
+<div className="flex w-screen h-screen justify-end align-start">
+        <div className="w-80 sm:w-1/2 static">
+            <img src="/personal-home.jpg" alt="Imagen de fondo" className="w-full h-full object-cover" />
+            <div className="px-6 w-1/2 absolute top-1/3 right-1/3 text-black">
+                <div className="w-screen sm:w-auto text-3xl font-bold">{texts.home.title}</div>
+                <div className="bg-amber-500 w-full h-2 mb-4"></div>
+                <div className="text-lg p-3 sm:p-6" id="subtitle-home">{texts.home.subtitle}</div>
+            </div>
+        </div>
 </div>
   );
 }
